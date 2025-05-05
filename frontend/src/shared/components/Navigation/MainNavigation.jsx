a2b84016124e291c7a0f4f84e6fdb89cb4c8456e
aleksmn <al-mn@yandex.ru>
2025-05-05 16:59:35 +0300

    fix style

--- frontend/src/shared/components/Navigation/MainNavigation.jsx
+++ frontend/src/shared/components/Navigation/MainNavigation.jsx
@@ -40,7 +40,7 @@ const MainNavigation = props => {
                 <h1 className="main-navigation__title">
                     <Link to="/">Turhub</Link>
                 </h1>
-                <nav>
+                <nav className="main-navigation__header-nav">
                     <NavLinks />
                 </nav>
             </MainHeader>
