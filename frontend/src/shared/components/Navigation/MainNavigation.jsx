1d78f13cffeefaa724022450d85eb4b818440e24
aleksmn <al-mn@yandex.ru>
2025-05-05 11:21:44 +0300

    add drawer with sidelinks

--- frontend/src/shared/components/Navigation/MainNavigation.jsx
+++ frontend/src/shared/components/Navigation/MainNavigation.jsx
@@ -1,23 +1,47 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
 import './MainNavigation.css';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Turhub</Link>
-      </h1>
-      <nav>
-        ...
-      </nav>
-    </MainHeader>
+    <>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Turhub</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
---
+++ frontend/src/shared/components/Navigation/NavLinks.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavLinks.css';
+
+const NavLinks = props => {
+  return <ul className="nav-links">
+    <li>
+      <NavLink to="/" exact="true">Все пользователи</NavLink>
+    </li>
+    <li>
+      <NavLink to="/u1/places">Мои места</NavLink>
+    </li>
+    <li>
+      <NavLink to="/places/new">Добавить место</NavLink>
+    </li>
+    <li>
+      <NavLink to="/auth">Войти</NavLink>
+    </li>
+  </ul>
+};
+
+export default NavLinks;
