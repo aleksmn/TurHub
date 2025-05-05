e4609e0425db44269fa5864bff47ff35af020e9a
aleksmn <al-mn@yandex.ru>
2025-05-05 17:14:33 +0300

    fix colors

--- frontend/src/users/pages/Users.jsx
+++ frontend/src/users/pages/Users.jsx
@@ -1,25 +1,35 @@
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-    const USERS = [
-        {
-          id: 'u1',
-          name: 'Дмитрий',
-          image:
-            'https://randomuser.me/api/portraits/men/81.jpg',
-          places: 3
-        },
-        {
-          id: 'u2',
-          name: 'Василий',
-          image:
-            'https://randomuser.me/api/portraits/men/82.jpg',
-          places: 5
-        }
-      ];
-    return ( 
-        <UsersList items={USERS}/>
-     );
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Дмитрий',
+      image: 'https://randomuser.me/api/portraits/men/81.jpg',
+      places: 3
+    },
+    {
+      id: 'u2',
+      name: 'Василий',
+      image: 'https://randomuser.me/api/portraits/men/82.jpg',
+      places: 5
+    },
+    {
+      id: 'u3',
+      name: 'Степан',
+      image: 'https://randomuser.me/api/portraits/men/85.jpg',
+      places: 4
+    },
+    {
+      id: 'u4',
+      name: 'Юлия',
+      image: 'https://randomuser.me/api/portraits/women/84.jpg',
+      places: 10
+    },
+  ]
+  return (
+    <UsersList items={USERS} />
+  );
 }
- 
+
 export default Users;
