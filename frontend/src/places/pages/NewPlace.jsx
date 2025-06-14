507d9550d56ba9ab271f856b2856a99940b64040
aleksmn <al-mn@yandex.ru>
2025-06-14 12:36:17 +0300

    add form component

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,9 +1,12 @@
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+
 const NewPlace = () => {
     return (
-        <div className="center">
-            <h2>Добавить место</h2>
-        </div>
+        <form className="place-form">
+            <Input element="input" type="text" label="Title" />
+        </form>
     );
-}
+};
 
 export default NewPlace;
