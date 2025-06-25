9e0fa2249b890322fb5289f1e7dac98e58157c2b
aleksmn <al-mn@yandex.ru>
2025-06-25 10:54:16 +0300

    add validators to form

--- frontend/src/places/pages/NewPlace.jsx
+++ frontend/src/places/pages/NewPlace.jsx
@@ -1,16 +1,31 @@
+import { useCallback } from 'react';
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/utils/validators';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/utils/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+    const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
     return (
         <form className="place-form">
             <Input
+                id="title"
                 element="input"
                 type="text"
                 label="Название"
                 validators={[VALIDATOR_REQUIRE()]}
-                errorText="Введите название места" />
+                errorText="Введите название места"
+                onInput={titleInputHandler} />
+            <Input
+                id="description"
+                element="textarea"
+                type="text"
+                label="Описание"
+                validators={[VALIDATOR_MINLENGTH(10)]}
+                errorText="Введите описание места"
+                onInput={descriptionInputHandler} />
         </form>
     );
 };
