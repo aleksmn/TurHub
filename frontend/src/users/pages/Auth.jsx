ead3d9c285baecf644b42f767393aa370a551c48
aleksmn <al-mn@yandex.ru>
2025-07-30 11:14:21 +0300

    swith to sign up button

--- frontend/src/users/pages/Auth.jsx
+++ frontend/src/users/pages/Auth.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import {
   VALIDATOR_EMAIL,
-  VALIDATOR_MINLENGTH
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
 } from '../../shared/utils/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import './Auth.css';
 
 const Auth = () => {
-  const [formState, inputHandler] = useForm(
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
         value: '',
@@ -23,6 +26,30 @@ const Auth = () => {
     false
   );
 
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: '',
+            isValid: false
+          }
+        },
+        false
+      );
+    }
+    setIsLoginMode(prevMode => !prevMode);
+  };
+
   const authSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs);
@@ -30,9 +57,20 @@ const Auth = () => {
 
   return (
     <Card className="authentication">
-      <h2>Авторизация</h2>
+      <h2 className="authentication__header">{isLoginMode ? 'Вход на сайт' : 'Регистрация'}</h2>
       <hr />
       <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            element="input"
+            id="name"
+            type="text"
+            label="Имя"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Введите имя"
+            onInput={inputHandler}
+          />
+        )}
         <Input
           element="input"
           id="email"
@@ -52,9 +90,12 @@ const Auth = () => {
           onInput={inputHandler}
         />
         <Button type="submit" disabled={!formState.isValid}>
-          LOGIN
+          {isLoginMode ? 'Войти' : 'Зарегистрироваться'}
         </Button>
       </form>
+      <Button inverse onClick={switchModeHandler}>
+        Переключить на {isLoginMode ? 'регистрацию' : 'вход'}
+      </Button>
     </Card>
   );
 };
