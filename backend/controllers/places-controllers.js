7a4753e6b82bbab7442c077daf49dc7c247cc9b3
aleksmn <al-mn@yandex.ru>
2025-09-15 16:28:38 +0300

    add post route

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,30 +1,35 @@
 const HttpError = require('../models/http-error');
+const { v4: uuid } = require('uuid');
+
+
+
+
 
 const PLACES = [
-    {
-        id: 'p1',
-        title: 'Крепость Орешек',
-        description: 'Крепость на острове Ореховом, напротив города Шлиссельбург',
-        image: 'oreshek.jpg',
-        address: 'остров Ореховый, Шлиссельбург, Ленинградская область',
-        location: {
-            lat: 59.953919,
-            lng: 31.039322,
-        },
-        creator: 'u1'
+  {
+    id: 'p1',
+    title: 'Крепость Орешек',
+    description: 'Крепость на острове Ореховом, напротив города Шлиссельбург',
+    image: 'oreshek.jpg',
+    address: 'остров Ореховый, Шлиссельбург, Ленинградская область',
+    location: {
+      lat: 59.953919,
+      lng: 31.039322,
     },
-    {
-        id: 'p2',
-        title: 'Кронштадт',
-        description: 'Город-порт в России, расположенный на острове Котлин',
-        image: 'kronshtadt.jpg',
-        address: 'остров Котлин, Кронштадт, Санкт-Петербург',
-        location: {
-            lat: 59.991920,
-            lng: 29.775658,
-        },
-        creator: 'u2'
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Кронштадт',
+    description: 'Город-порт в России, расположенный на острове Котлин',
+    image: 'kronshtadt.jpg',
+    address: 'остров Котлин, Кронштадт, Санкт-Петербург',
+    location: {
+      lat: 59.991920,
+      lng: 29.775658,
     },
+    creator: 'u2'
+  },
 
 ];
 
@@ -59,5 +64,23 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  // const title = req.body.title;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+
+  PLACES.push(createdPlace);  
+
+  res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
@@ -7,6 +9,7 @@ require("dotenv").config();
 const placesRoutes = require("./routes/places-routes");
 
 // middleware
+// app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors({
     origin: process.env.FRONTEND_URL
@@ -28,8 +31,12 @@ app.get("/", (req, res) => {
     res.status(201).json({message: "Connected to Backend!"});
 });
 
-
 app.use('/api/places', placesRoutes);
 
-
-
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
+});
